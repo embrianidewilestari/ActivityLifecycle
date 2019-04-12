72d14fcac904279c52365d0ac6ba376c9ce310ed
Embriani Dewi Lestari <embrianidl13@gmail.com>
2019-04-12 11:38:14 +0700

    ActivityLifecycle

---
+++ Jobsheet2_LatihanPraktikum/app/src/main/java/com/example/latihanpraktikum_embrianidl/MainActivity.java
@@ -0,0 +1,15 @@
+package com.example.latihanpraktikum_embrianidl;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_linear);
+        setContentView(R.layout.activity_constraint);
+    }
+}
